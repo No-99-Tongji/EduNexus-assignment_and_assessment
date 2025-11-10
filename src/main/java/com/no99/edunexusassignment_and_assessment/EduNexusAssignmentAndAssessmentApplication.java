91e519c08323a5aeb133ca5904cb3c5357729172
No-99-Tongji <3167937401@qq.com>
2025-11-10 12:38:01 +0800

    feat: enable Nacos configuration and Spring Cloud features in application properties

--- src/main/java/com/no99/edunexusassignment_and_assessment/EduNexusAssignmentAndAssessmentApplication.java
+++ src/main/java/com/no99/edunexusassignment_and_assessment/EduNexusAssignmentAndAssessmentApplication.java
@@ -3,15 +3,11 @@ package com.no99.edunexusassignment_and_assessment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {
-        com.alibaba.cloud.nacos.discovery.NacosDiscoveryAutoConfiguration.class,
-        com.alibaba.cloud.nacos.registry.NacosServiceRegistryAutoConfiguration.class,
-        com.alibaba.cloud.nacos.NacosConfigAutoConfiguration.class,
-        org.springframework.cloud.client.serviceregistry.ServiceRegistryAutoConfiguration.class
-})
+@SpringBootApplication
 @MapperScan("com.no99.edunexusassignment_and_assessment.mapper")
-// @EnableDiscoveryClient - Disabled when Nacos is not available
+@EnableDiscoveryClient
 public class EduNexusAssignmentAndAssessmentApplication {
 
     public static void main(String[] args) {
